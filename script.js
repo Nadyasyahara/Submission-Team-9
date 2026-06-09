41ed7472b4275aba3fd3bbdd72689901d3aa16d7
Tiar <ikhtiarmandru34@gmail.com>
2026-06-10 06:04:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const Pilihan = document.getElementById("Pilihan");
-        const Menu = document.getElementById("Menu");
+const Pilihan = document.getElementById('Pilihan');
+const semuakategori = document.querySelectorAll('.Kategori-menu');
 
-        Pilihan.addEventListener("change", function(e){
-            Menu.innerText = `menu yang tersedia: ${e.target.value}`;
+        Pilihan.addEventListener('change', function(){
+            const pilihanuser = this.value;
+            semuakategori.forEach(Kategori => {
+                if (pilihanuser === 'Semua' || Kategori.dataset.kategori === pilihanuser) {
+                    Kategori.style.display = 'block';
+                } else {
+                    Kategori.style.display = 'none';
+                }
+            });
         });
