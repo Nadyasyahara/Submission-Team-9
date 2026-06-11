ff4a06d9967364b45172c59c146da72f2e5ebc75
Tiar <ikhtiarmandru34@gmail.com>
2026-06-11 22:01:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const Pilihan = document.getElementById('Pilihan');
 const semuakategori = document.querySelectorAll('.Kategori-menu');
 
-        Pilihan.addEventListener('change', function(){
-            const pilihanuser = this.value;
-            semuakategori.forEach(Kategori => {
-                if (pilihanuser === 'Semua' || Kategori.dataset.kategori === pilihanuser) {
-                    Kategori.style.display = 'block';
-                } else {
+if (Pilihan) {
+    Pilihan.addEventListener('change', function () {
+        const pilihanuser = this.value;
+        semuakategori.forEach(Kategori => {
+            if (pilihanuser === 'Semua') {
+                if (Kategori.dataset.kategori === 'Favorit') {
                     Kategori.style.display = 'none';
+                } else {
+                    Kategori.style.display = 'block';
                 }
-            });
+            } else if (Kategori.dataset.kategori === pilihanuser) {
+                Kategori.style.display = 'block';
+            } else {
+                Kategori.style.display = 'none';
+            }
         });
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        semuakategori.forEach(Kategori => {
+            if (Kategori.dataset.kategori === 'Favorit') {
+                Kategori.style.display = 'none';
+            }
+        });
+    });
+}
+
+const menu = document.getElementById("menuhp");
+const openBtn = document.getElementById("menu-toggle");
+const closeBtn = document.getElementById("close-menu");
+
+if (menu && openBtn && closeBtn) {
+    openBtn.addEventListener("click", () => {
+        menu.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+}
